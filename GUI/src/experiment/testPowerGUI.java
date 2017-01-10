3747e885e039723c09bb257cdfbbc57f00aada28
d-acharya <dines.acharya@gmail.com>
2017-01-10 14:16:37 +0100

    GUI SFINA

---
+++ GUI/src/experiment/testPowerGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package experiment;
+
+import gui.*;
+
+/**
+ *
+ * @author dinesh
+ */
+public class testPowerGUI {
+    public static void main(String agrs[]){
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new PowerFlowCascadeGUI("Case200RandomExperiment").setVisible(true);
+            }
+        });
+    }
+}
---
+++ GUI/src/gui/TreeMouseListener.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
+import javax.swing.JTree;
+import javax.swing.tree.TreePath;
+
+/**
+ *
+ * @author dinesh
+ */
+public class TreeMouseListener extends MouseAdapter{
+    
+        JTree tree;
+        
+        public TreeMouseListener(JTree tree){
+            this.tree = tree;
+        }
+         
+        @Override
+        public void mousePressed(MouseEvent e) {
+            int selRow = tree.getRowForLocation(e.getX(), e.getY());
+            TreePath selPath = tree.getPathForLocation(e.getX(), e.getY());
+            if(selRow != -1) {
+                
+                if(e.getClickCount() == 2) {
+                    String filePath = "";
+                    for(int i =0;i<selPath.getPathCount()-1;i++){
+                        filePath = filePath+selPath.getPathComponent(i)+"/";
+                    }
+                    filePath += selPath.getPathComponent(selPath.getPathCount()-1);
+                    
+                    String columnSeperator = ",";
+                    boolean header = false;
+                    TabularDataEditor viewer = new TabularDataEditor(filePath, columnSeperator, header);
+                    
+                    JOptionPane.showConfirmDialog(null, viewer, "TabularDataViewer", JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE);
+                }
+            }
+        }
+    
+}
